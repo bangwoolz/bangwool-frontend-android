4ef6645c599a20aa5c67663f2cae32c35d950514
Mouon <ahemsapsldk@gmail.com>
2023-07-12 22:30:29 +0900

    A-14 png-to-xml,layout-fixed

--- app/src/main/java/com/example/bangwool/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/bangwool/ui/mypage/MyPageFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.bangwool.LoginActivity
 import com.example.bangwool.databinding.FragmentMypageBinding
 
 class MyPageFragment : Fragment() {
