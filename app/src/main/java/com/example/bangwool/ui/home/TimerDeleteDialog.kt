da9420a3e10f83819a8972217bc4c150facfa6a0
Kjiw0n <rwd0904@naver.com>
2023-07-23 05:11:52 +0900

    BW-47 homeMenuDialog 추가

--- app/src/main/java/com/example/bangwool/ui/home/TimerDeleteDialog.kt
+++ app/src/main/java/com/example/bangwool/ui/home/TimerDeleteDialog.kt
@@ -5,13 +5,9 @@ import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.example.bangwool.R
 import com.example.bangwool.databinding.DialogTimerDeleteBinding
-import com.google.gson.Gson
 
 class TimerDeleteDialog : DialogFragment() {
     lateinit var binding: DialogTimerDeleteBinding
@@ -57,35 +53,4 @@ class TimerDeleteDialog : DialogFragment() {
 
         return builder.create()
     }
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        binding = DialogTimerDeleteBinding.inflate(inflater, container, false)
-////        binding.dialogCl.setBackgroundResource(R.drawable.dialog_layout)
-//        val builder = AlertDialog.Builder(requireActivity(), R.style.RoundedDialogTheme)
-//            .setView(binding.root)
-//
-//        val dataJson = arguments?.getString("taskData")
-//        if (dataJson != null) {
-////            val data = Gson().fromJson(dataJson, HomeItem::class.java)
-//            Log.d("taskData", dataJson.toString())
-//            val originalText = binding.dialogTv.text.toString()
-////            val newText = originalText.replace("\"taskName\"", dataJson.toString())
-//            val newText = originalText.replace("taskName", dataJson.toString())
-//            binding.dialogTv.text = newText
-//        }
-//
-//        binding.cancelBtn.setOnClickListener {
-//            dismiss()
-//        }
-//        binding.deleteBtn.setOnClickListener {
-//
-//        }
-//
-//
-//
-//        return builder.create()
-//    }
 }
