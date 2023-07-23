e2d9cab03fd1e45ae138e1c541c283d10591a548
Unique0902 <rla0591@naver.com>
2023-07-23 21:27:41 +0900

    Feat: make color select

--- app/src/main/java/com/example/bangwool/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/bangwool/ui/home/HomeFragment.kt
@@ -17,7 +17,6 @@ import com.example.bangwool.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
-    var itemList: ArrayList<HomeItem> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,57 +24,10 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-        binding.homeRecyclerView.setOnClickListener {
-            val intent = Intent(activity, TimerActivity::class.java)
-            startActivity(intent)
-        }
 
-        initDummyData()
-        init()
+
         return binding.root
     }
 
-    fun init() {
-        binding.apply {
-//            homeRecyclerView.layoutManager =
-//                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-//            homeRecyclerView.adapter = HomeAdapter(requireContext(), itemList)
 
-            val swipeHelperCallback = SwipeHelperCallback().apply {
-                setClamp(dpToPx(160f, requireContext()))
-            }
-            val itemTouchHelper = ItemTouchHelper(swipeHelperCallback)
-            itemTouchHelper.attachToRecyclerView(homeRecyclerView)
-
-            homeRecyclerView.apply {
-                layoutManager =
-                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-                adapter = HomeAdapter(requireContext(), itemList)
-
-                setOnTouchListener { _, _ ->
-                    swipeHelperCallback.removePreviousClamp(this)
-                    false
-                }
-            }
-
-            homeAddTaskBtn.setOnClickListener {
-                Toast.makeText(requireContext(), "+ 버튼 클릭됨", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    // dp 값을 px 값으로 변환하는 함수
-    fun dpToPx(dp: Float, context: Context): Float {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
-    }
-
-
-    fun initDummyData() {
-        val dummydata = HomeItem(R.color.primary_100, "잠자기", "03:33", 0)
-        itemList.add(dummydata)
-        itemList.add(dummydata)
-        itemList.add(dummydata)
-        itemList.add(dummydata)
-        itemList.add(dummydata)
-        itemList.add(dummydata)
-    }
 }
