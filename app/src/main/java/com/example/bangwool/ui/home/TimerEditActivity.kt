93c8747cff7bcacf3173c8e2c5fca2d9dc580597
nonaninona <starcraft0529@gmail.com>
2023-07-25 19:11:36 +0900

    timer edit branch merge

--- app/src/main/java/com/example/bangwool/ui/home/TimerEditActivity.kt
+++ app/src/main/java/com/example/bangwool/ui/home/TimerEditActivity.kt
@@ -3,10 +3,16 @@ package com.example.bangwool.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bangwool.R
+import com.example.bangwool.databinding.ActivityTimerEditBinding
 
 class TimerEditActivity : AppCompatActivity() {
+    lateinit var binding : ActivityTimerEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer_edit)
+        binding = ActivityTimerEditBinding.inflate(layoutInflater)
+        binding.icTimerEditBack.setOnClickListener {
+            finish()
+        }
+        setContentView(binding.root)
     }
 }
