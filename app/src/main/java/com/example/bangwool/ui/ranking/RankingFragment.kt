18504198e58b8c4f78657bb9f8312cf0ac11ec6a
nonaninona <starcraft0529@gmail.com>
2023-07-25 19:23:36 +0900

    ranking branch merge

--- app/src/main/java/com/example/bangwool/ui/ranking/RankingFragment.kt
+++ app/src/main/java/com/example/bangwool/ui/ranking/RankingFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.bangwool.databinding.FragmentRankingBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class RankingFragment : Fragment() {
     lateinit var binding : FragmentRankingBinding
