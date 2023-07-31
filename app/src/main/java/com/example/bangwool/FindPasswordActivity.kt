2efb2886b43fa464a6f38236e856c499d1b03de9
Mouon <ahemsapsldk@gmail.com>
2023-07-31 20:45:06 +0900

    BW-90:FindPwActivity-modify

--- app/src/main/java/com/example/bangwool/FindPasswordActivity.kt
+++ app/src/main/java/com/example/bangwool/FindPasswordActivity.kt
@@ -62,6 +62,7 @@ class FindPasswordActivity : AppCompatActivity() {
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 }
 
+
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                     // 이름이 변경된 후에 버튼 상태 업데이트
                     updateButtonState()
