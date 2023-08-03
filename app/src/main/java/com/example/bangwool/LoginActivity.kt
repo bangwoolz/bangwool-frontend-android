3a4074d8b50514795d8e3ce8e23eaca5079f4ac1
nonaninona <starcraft0529@gmail.com>
2023-08-04 01:08:30 +0900

    loginActivity 로직 수정, 에러 아이콘 등 수정

--- app/src/main/java/com/example/bangwool/LoginActivity.kt
+++ app/src/main/java/com/example/bangwool/LoginActivity.kt
@@ -20,7 +20,9 @@ import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
-    companion object co{
+    private var isIdValid = false
+
+    companion object co {
         var activity: LoginActivity? = null
     }
 
@@ -36,80 +38,71 @@ class LoginActivity : AppCompatActivity() {
     @SuppressLint("ClickableViewAccessibility")
     private fun init() {
         binding.apply {
-
+            loginCl.requestFocus()
+            idTextInputLayout.hint = null
+            idTextInputLayout.setErrorIconDrawable(null)
+            idTextInputLayout.boxStrokeErrorColor = getColorStateList(R.color.secondary)
+            loginIdEt.hint = "ex) banwol@gmail.com"
 
             loginIdEt.setOnFocusChangeListener { _, hasFocus ->
-                if (hasFocus) {
-                    idTextInputLayout.hint = null
-                    idTextInputLayout.requestFocus()
+                if (!hasFocus && loginIdEt.text.isNullOrEmpty()) {
+                    loginIdEt.hint = "ex) banwol@gmail.com"
                 } else {
-                    if (loginIdEt.text.isNullOrEmpty()) {
-                        idTextInputLayout.hint = "이메일을 입력하세요. "
-                    } else {
-                        idTextInputLayout.hint = null
-                    }
+                    loginIdEt.hint = null
+                }
+
+                if (hasFocus) {
+                    loginIdEt.setTextColor(getColor(R.color.black))
+                } else {
+                    loginIdEt.setTextColor(getColor(R.color.gray_700))
                 }
             }
 
-            loginStartBtn.setBackgroundResource(R.drawable.long_normal_btn)
-            loginStartBtn.backgroundTintList = getColorStateList(R.color.gray_300)
-
             loginIdEt.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    idTextInputLayout.hint = null
+                    validateId(s.toString().trim())
+                    updateButtonState()
                 }
 
                 override fun afterTextChanged(s: Editable?) {
-                    val id = s.toString().trim()
-
-//                    if (id.isEmpty()){
-//                        idTextInputLayout.error = "잘못된 이메일 형식이에요"
-//                    }
-
-                    if (isValidId(id)) {
-                        updateEndIcon(true)
-                        loginIcErrorEmail.visibility = View.GONE
-                        loginStartBtn.setBackgroundResource(R.drawable.long_normal_btn)
-                        loginStartBtn.backgroundTintList = getColorStateList(R.color.primary)
-
-                        idTextInputLayout.error = null
-                        idTextInputLayout.hint = null
-                        loginStartBtn.setOnClickListener {
-                            //error Icon 삭제
-                            idTextInputLayout.setErrorIconDrawable(null)
-                            // ProgressBar 보이도록 설정
-                            loginProgressBar.visibility = View.VISIBLE
-                            // 일정 시간(300ms) 후에 체크 이미지로 변경
-                            Handler(Looper.getMainLooper()).postDelayed({
-                                loginProgressBar.visibility = View.GONE
-                                loginLoadingDone.visibility = View.VISIBLE
-                                //로딩 완료 메세지
-                                idTextInputLayout.error = "잠시후 로그인 창으로 이동합니다"
-                                //에러메세지 색상 변경
-                                idTextInputLayout.setErrorTextAppearance(R.style.CustomTextInputLayout)
-                                idTextInputLayout.boxStrokeErrorColor = getColorStateList(R.color.androidDefault)
-                                val intent = Intent(this@LoginActivity, PasswordActivity::class.java)
-                                intent.putExtra("loginId", id)
-                                startActivity(intent)
-                                idTextInputLayout.error = null;
-                                loginLoadingDone.visibility = View.GONE
-                            }, 2000)
-
-
-
-                        }
-                    } else {
-                        updateEndIcon(false)
-                        loginIcErrorEmail.visibility = View.VISIBLE
-                        loginStartBtn.setBackgroundResource(R.drawable.long_normal_btn)
-                        loginStartBtn.backgroundTintList = getColorStateList(R.color.gray_300)
-                        idTextInputLayout.error = "     잘못된 이메일 형식이에요"
-                    }
                 }
-
             })
+
+            loginStartBtn.setBackgroundResource(R.drawable.long_normal_btn)
+            loginStartBtn.backgroundTintList = getColorStateList(R.color.gray_300)
+            loginStartBtn.setOnClickListener {
+                //error Icon 삭제
+                idTextInputLayout.setErrorIconDrawable(null)
+                // ProgressBar 보이도록 설정
+                loginProgressBar.visibility = View.VISIBLE
+
+                // 일정 시간(300ms) 후에 체크 이미지로 변경
+                Handler(Looper.getMainLooper()).postDelayed({
+                    loginProgressBar.visibility = View.GONE
+                    loginLoadingDone.visibility = View.VISIBLE
+                    //로딩 완료 메세지
+                    idTextInputLayout.error = "잠시후 로그인 창으로 이동합니다"
+                    //에러메세지 색상 변경
+                    idTextInputLayout.setErrorTextAppearance(R.style.CustomTextInputLayout)
+                    idTextInputLayout.boxStrokeErrorColor =
+                        getColorStateList(R.color.androidDefault)
+                    val intent =
+                        Intent(this@LoginActivity, PasswordActivity::class.java)
+                    val id = loginIdEt.text
+                    intent.putExtra("loginId", id)
+                    startActivity(intent)
+                    idTextInputLayout.error = null;
+                    loginLoadingDone.visibility = View.GONE
+                }, 2000)
+            }
 
             loginRegisterBtn.setOnClickListener {
                 val i = Intent(this@LoginActivity, RegisterActivity::class.java)
@@ -118,48 +111,51 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    /*
-    private fun validateEmail(email: String): Boolean {
-        val emailPattern = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+//    private fun validateId(id: String) {
+//        if (isValidId(id)) {
+//            updateEndIcon(true)
+//            binding.apply {
+//                loginIcErrorEmail.visibility = View.GONE
+//                loginStartBtn.setBackgroundResource(R.drawable.long_normal_btn)
+//                idTextInputLayout.error = null
+//                idTextInputLayout.hint = null
+//            }
+//
+//        } else {
+//            updateEndIcon(false)
+//            binding.apply {
+//                loginIcErrorEmail.visibility = View.VISIBLE
+//                loginStartBtn.setBackgroundResource(R.drawable.long_normal_btn)
+//                loginStartBtn.backgroundTintList = getColorStateList(R.color.gray_300)
+//                idTextInputLayout.error = "     잘못된 이메일 형식이에요"
+//            }
+//        }
+//    }
 
-        val grayColor = ContextCompat.getColor(this, R.color.gray_600)
-        if (email.isEmpty()) {
-            binding.textInputLayoutEmail.error = "이메일을 입력하세요."
-            binding.textInputLayoutEmail.isErrorEnabled = true
-            updateEndIcon(false)
-            return false
-        } else if (!emailPattern.matcher(email).matches()) {
-            binding.textInputLayoutEmail.error = "올바른 이메일 형식이 아니에요"
-            binding.textInputLayoutEmail.isErrorEnabled = true
-            updateEndIcon(false)
-            return false
-        } else {
-            binding.textInputLayoutEmail.error = null
-            binding.textInputLayoutEmail.isErrorEnabled = true
-            binding.textInputLayoutConfirmPassword.boxStrokeErrorColor = ColorStateList.valueOf(grayColor)
-            updateEndIcon(true)
-            return true
-        }
-    }
-     */
-    private fun isValidId(email: String): Boolean {
+    private fun validateId(email: String): Boolean {
         val emailPattern = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
         val grayColor = ContextCompat.getColor(this, R.color.gray_300)
         if (email.isEmpty()) {
-            binding.idTextInputLayout.error = "이메일을 입력하세요."
+            binding.idTextInputLayout.error = "          이메일을 입력하세요."
             binding.idTextInputLayout.isErrorEnabled = true
+            binding.loginIcErrorEmail.visibility = View.VISIBLE
             updateEndIcon(false)
+            isIdValid = false
             return false
         } else if (!emailPattern.matcher(email).matches()) {
-            binding.idTextInputLayout.error = "올바른 이메일 형식이 아니에요"
+            binding.idTextInputLayout.error = "          올바른 이메일 형식이 아니에요"
             binding.idTextInputLayout.isErrorEnabled = true
+            binding.loginIcErrorEmail.visibility = View.VISIBLE
             updateEndIcon(false)
+            isIdValid = false
             return false
         } else {
             binding.idTextInputLayout.error = null
-            binding.idTextInputLayout.isErrorEnabled = true
+            binding.idTextInputLayout.isErrorEnabled = false
             binding.idTextInputLayout.boxStrokeErrorColor = ColorStateList.valueOf(grayColor)
+            binding.loginIcErrorEmail.visibility = View.GONE
             updateEndIcon(true)
+            isIdValid = true
             return true
         }
     }
@@ -169,18 +165,33 @@ class LoginActivity : AppCompatActivity() {
             if (isValid) {
                 ContextCompat.getDrawable(this, R.drawable.round_check_24)
             } else {
-                ContextCompat.getDrawable(this, R.drawable.ic_error_circle_outline)
+                null
             }
 
         if (isValid) {
             val tintColor = ContextCompat.getColor(this, R.color.gray_700)
             endIconDrawable?.setColorFilter(tintColor, PorterDuff.Mode.SRC_IN)
-        }else{
+        } else {
             val tintColor = ContextCompat.getColor(this, R.color.secondary)
             endIconDrawable?.setColorFilter(tintColor, PorterDuff.Mode.SRC_IN)
         }
         binding.idTextInputLayout.setErrorIconDrawable(endIconDrawable)
+    }
 
+    private fun updateButtonState() {
+        val isFormValid = isIdValid
+
+        binding.loginStartBtn.isEnabled = isFormValid
+        if (isFormValid) {
+            val enabledColor =
+                ContextCompat.getColorStateList(this@LoginActivity, R.color.enabledColor)
+            binding.loginStartBtn.backgroundTintList = enabledColor
+
+        } else {
+            val disabledColor =
+                ContextCompat.getColorStateList(this@LoginActivity, R.color.disabledColor)
+            binding.loginStartBtn.backgroundTintList = disabledColor
+        }
     }
 
 }
