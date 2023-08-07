59239581f3a3bb9b1f9ea6603c7c9d9690295070
Mouon <ahemsapsldk@gmail.com>
2023-08-08 01:42:08 +0900

    BW-115:주간,일간 랭킹 정보 api연

--- app/src/main/java/com/example/bangwool/ui/ranking/RankingDayFragment.kt
+++ app/src/main/java/com/example/bangwool/ui/ranking/RankingDayFragment.kt
@@ -48,6 +48,7 @@ class RankingDayFragment : Fragment() {
         RetrofitUtil.setAccessToken(getAccessToken(requireContext()))
         val apiService = RetrofitUtil.getRetrofit()
 
+        // 현재 시간 , 오전 6시 시간 계산
         val currentTime = Calendar.getInstance().timeInMillis
         val sixAM = Calendar.getInstance().apply {
             set(Calendar.HOUR_OF_DAY, 6)
@@ -56,28 +57,44 @@ class RankingDayFragment : Fragment() {
             set(Calendar.MILLISECOND, 0)
         }.timeInMillis
 
-        val start = if (currentTime < sixAM) 0 else ((currentTime - sixAM) / (1000 * 60)).toInt()
-        val end = start
+        // 랭킹 조회 범위 계산
+        val start = if (currentTime < sixAM) 0
+            // 만약 현재 시간이 오전 6시 이전이라면 랭킹 조회 시작 시간을 0으로 설정
+            // 오전 6시 이전에는 랭킹 데이터를 업데이트하지 않으..
+        else ((currentTime - sixAM) / (1000 * 60)).toInt()
+            // 현재 시간이 오전 6시 이후라면 시간 차이를 분 단위로 계산하여 시작 시간으로 설정함
+        val end = if (currentTime < sixAM) ((sixAM - currentTime) / (1000 * 60)).toInt()
+            // 만약 현재 시간이 오전 6시 이전이라면 랭킹 조회 끝 시간을 현재 시간과의 시간 차이로 설정
+        else 24 * 60 - ((currentTime - sixAM) / (1000 * 60)).toInt() - 1
+        // 현재 시간이 오전 6시 이후라면 랭킹 조회 끝 시간을 오전 5시 59분까지의 분으로 설정
+        //하루 전체 시간을 대상으로 하지만 현재 시간을 기준으로 랭킹을 조회하므로, 끝 시간을 설정할 때에는 24시간에서 오전 6시부터 현재 시간까지의 시간 차이를 뺌
 
+        // 랭킹 조회 요청 보내기
         apiService.getDailyRanking(DailyRankingRequest(start, end)).enqueue(object : Callback<RankingResponse> {
             override fun onResponse(call: Call<RankingResponse>, response: Response<RankingResponse>) {
                 if (response.isSuccessful) {
                     val rankingItems = response.body()?.rankingResponses
+                    // 기존에 저장되어 있던 랭킹 정보를 모두 지우고 초기화
                     rankingList.clear()
                     rankingItems?.let {
                         for (item in it) {
+                            // 랭킹 정보를 변환하여 리스트에 추가
                             rankingList.add(RankingInfo(item.rank, 0, item.nickname, item.workedHour * 60 + item.workedMin))
                         }
                     }
+                    // 어댑터에 데이터 변경을 알리고 리사이클러뷰 업데이트
                     adapter.notifyDataSetChanged()
                 } else {
+                    // API 응답 실패 시 로그 출력
                     Log.e("RankingWeekFragment", "API 응답 실패: ${response.code()}")
                 }
             }
 
             override fun onFailure(call: Call<RankingResponse>, t: Throwable) {
+                // 통신 실패 시 로그 출력
                 Log.e("RankingWeekFragment", "통신 실패: ${t.message}")
             }
         })
+
     }
 }
--- app/src/main/java/com/example/bangwool/ui/ranking/RankingWeekFragment.kt
+++ app/src/main/java/com/example/bangwool/ui/ranking/RankingWeekFragment.kt
@@ -39,19 +39,29 @@ class RankingWeekFragment : Fragment() {
         binding.rvWeekRanking.adapter = adapter
     }
 
+    //시간 범위를 계산하고 이용하기에 용이하데서 Calendar 클래스사용..
+    //날짜 다뤄야해서
     fun fetchWeeklyRankingData() {
         RetrofitUtil.setAccessToken(getAccessToken(requireContext()))
         val apiService = RetrofitUtil.getRetrofit()
 
+        //현재시간 밀리초로 가져옴
         val currentTime = Calendar.getInstance().timeInMillis
+        //Calendar 객체를 생성
         val cal = Calendar.getInstance()
+        //Calendar 객체의 요일을 월요일로 설정해서 주간 랭킹의 시작 시간을 현재 주의 월요일로 설정..매주 월요일부터 랭킹 시작
         cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        //Calendar 객체의 시간 정보를 0시 0분 0초로 설정, 매주 월요일 자정부터 시작된데요
         cal.set(Calendar.HOUR_OF_DAY, 0)
         cal.set(Calendar.MINUTE, 0)
         cal.set(Calendar.SECOND, 0)
+        //Calendar 객체의 시간 정보를 밀리초 단위로 가져와서 주간 랭킹의 시작 시간으로 설정
         val start = cal.timeInMillis
+        //주간 랭킹의 끝 시간으로 현재 시간을 사용..12시부터 현재시간을 주간으로 쓰니까
         val end = currentTime
+        //서버로 시작,끝시간 주기 끝..
 
+        //enqueue 메서드를 호출하는부분은 구글링..
         apiService.getWeeklyRanking(WeeklyRankingRequest(start.toInt(), end.toInt())).enqueue(object : Callback<RankingResponse> {
             override fun onResponse(call: Call<RankingResponse>, response: Response<RankingResponse>) {
                 if (response.isSuccessful) {
@@ -64,11 +74,13 @@ class RankingWeekFragment : Fragment() {
                     }
                     adapter.notifyDataSetChanged()
                 } else {
+                    //로그 찍기1
                     Log.e("RankingWeekFragment", "API 응답 실패: ${response.code()}")
                 }
             }
 
             override fun onFailure(call: Call<RankingResponse>, t: Throwable) {
+                //로그 찍기2
                 Log.e("RankingWeekFragment", "통신 실패: ${t.message}")
             }
         })
