b7221a6be3829c78ffd8caf93a1e77e293d3dbf2
Unique0902 <rla0591@naver.com>
2023-08-27 20:07:25 +0900

    Feat: make editPrivacy layout

---
+++ app/src/main/java/com/example/bangwool/ui/mypage/EditPrivacyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bangwool.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bangwool.R
+
+class EditPrivacyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_privacy)
+    }
+}
